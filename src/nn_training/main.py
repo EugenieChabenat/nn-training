1baf01dd274e000cd9614dbb5bab1d089a24a46c
EugenieChabenat <44575103+EugenieChabenat@users.noreply.github.com>
2023-03-06 10:47:06 -0500

    Update main.py

--- src/nn_training/main.py
+++ src/nn_training/main.py
@@ -18,9 +18,11 @@ import torch.utils.data.distributed
 from torch.utils.tensorboard import SummaryWriter
 
 # original line
-from nn_training import config, nn_modules
+#from nn_training import config, nn_modules
 
 #--added
+import config 
+import nn_modules
 #from src.nn_training.nn_modules import supervised
 #from nn-training.src.nn_training import config, nn_modules
 #import sys
